877db68e61ec00a3bb06598070d814f877205ad2
Antonio Ruggiero <antonrugg@gmail.com>
2022-04-18 15:26:30 +0200

    added task logics

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
 <div class="container">
-  <Header title="Task Tracker"/>  
+  <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask"/>  
+  <div v-show="showAddTask">
+    <AddTask @add-task="addTask" />
+  </div>
   <Tasks @delete-task="deleteTask"
          @toggle-reminder="toggleReminder" 
            :tasks="tasks" />
+  
 </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
 
 
 export default {
   name: "App",
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask,
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false
     }
   },
   methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask;
+    },
+
+    addTask(task){
+      this.tasks = [...this.tasks, task];
+    },
+
     deleteTask(id){
       if(confirm ('Are you sure?')){
         this.tasks = this.tasks.filter((task) => task.id !== id)
@@ -31,7 +46,6 @@ export default {
     },
     toggleReminder(id){
       this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
-      console.log({...this.task})
     }
   },
 
